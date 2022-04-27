4b69afcae3ba33abd1de6193a93069a790931bb2
Roahitvijay <100504788+Roahitvijay@users.noreply.github.com>
2022-04-27 21:23:03 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🎖 DEPLOY YOURS 🎖', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🎖 Group Search 🎖', url='https://t.me/animegroup99')
                     ]
                     ]
                 await bot.send_cached_media(
